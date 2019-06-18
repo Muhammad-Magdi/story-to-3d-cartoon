1290fab3856943af4bf63147afcba455c7f304cf
muhammad <Muhammad.magdi1024@gmail.com>
2019-06-18 12:02:54 +0200

    add a log function to report in the stderr stream

--- helpers/core_helper.py
+++ helpers/core_helper.py
@@ -7,9 +7,13 @@ import time
 TRIALS = 10
 SLEEPTIME = 1
 
+def log(message):
+    sys.stderr.write(message + '\n')
+    sys.stderr.flush()
+
 #properties = {"annotators": -list of comma separated annotators-, "outputFormat": "json"}
 def call_core(input, annotators):
-    print("Connecting to CoreNLP Server...")
+    log("Connecting to CoreNLP Server...")
 
     CORESERVER = 'http://corenlp.run'
     properties = (
@@ -22,18 +26,18 @@ def call_core(input, annotators):
             break
         except:
             trial += 1
-            print('Trial Number', trial, ' -> Connection Error: ', sys.exc_info()[0])
+            log('Trial Number '+  str(trial)+ ' -> Connection Error: '+ sys.exc_info()[0])
             time.sleep(SLEEPTIME)
     else:
         raise Exception("Couldn't Connect to the CoreNLP Server.")
     
-    print('CoreNLP Server Returned.')
+    log('CoreNLP Server Returned.')
 
     return json.loads(response.text)
 
 
 def tokenize(text):
-    print('Tokenizing...')
+    log('Tokenizing...')
     jsonRet = call_core(text, "tokenize,ssplit")['sentences']
     tokens = dict()
     for sent_id in range(len(jsonRet)):
@@ -43,12 +47,12 @@ def tokenize(text):
             sentDict[idx] = x['originalText']
             idx += 1
         tokens[sent_id] = sentDict
-    print('Tokenization done:', tokens)
+    log('Tokenization done: '+ str(tokens))
     return tokens
 
 
 def solve_coref(text):
-    print("Solving Coreferences...")
+    log("Solving Coreferences...")
     jsonRet = call_core(text, "coref,ssplit")
     tokens = tokenize(text)
     for num in jsonRet['corefs'].keys():
@@ -68,14 +72,14 @@ def solve_coref(text):
         for idx in range(len(tokens[sent_id])):
             ret.append(tokens[sent_id][idx])
     ret = ' '.join(ret)
-    print("Solving Coreferences done: ", ret)
+    log("Solving Coreferences done: "+ str(ret))
     return ret
 
 
 #The Dictionary structure is
 #   dict[sentence_number][phrase_number]
 def openie(text):
-    print('Parsing...')
+    log('Parsing...')
     jsonRet = call_core(text, 'openie')['sentences']
     ret = dict()
 
@@ -88,14 +92,16 @@ def openie(text):
             ret[sent['index']][index] = {key: phrase[key] for key in keys}
             index += 1
     
-    print('Parsing done:', ret)
+    log('Parsing done: '+ str(ret))
+    sys.stderr.flush()
     return ret
 
 def enhancedDependencies(text):
-    print('Finding Dependencies...')
+    log('Finding Dependencies...')
     jsonRet = call_core(text, 'parse')
     ret = dict()
-    print(jsonRet)
+    log(jsonRet)
+    sys.stderr.flush()
 
 #Returns a dictionary[original_text] that contains:
 #lemma => the lemma of the original word
@@ -103,7 +109,7 @@ def enhancedDependencies(text):
 #end => the ending index in the original text
 #pos => the part of speech
 def get_info(text):
-    print('Getting Lemmas...')
+    log('Getting Lemmas...')
     jsonRet = call_core(text, 'lemma')['sentences']
     ret_dict = dict()
     for sent in jsonRet:
@@ -113,5 +119,5 @@ def get_info(text):
             ret_dict[token['originalText']]['begin'] = token['characterOffsetBegin']
             ret_dict[token['originalText']]['end'] = token['characterOffsetEnd']
             ret_dict[token['originalText']]['pos'] = token['pos']
-    print('Lemmatizing done:', ret_dict)
+    log('Lemmatizing done: '+ str(ret_dict))
     return ret_dict
